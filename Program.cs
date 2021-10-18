7f9330f032abba007784c7daf1b092c48f72b44e
SokurenkoED <SokurenkoED@yandex.ru>
2021-10-18 11:07:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetListFromDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
